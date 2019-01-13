e544e0e04a0bef3a1fd278b7ad13b4efa8566a77
bglmmz <bglmmz@hotmail.com>
2019-01-14 00:56:57 +0800

    first commit

---
+++ draw_direct/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace draw_direct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(804, 454);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.SystemColors.Control;
+            this.btnNext.Location = new System.Drawing.Point(81, 1);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "下一页";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(0, 1);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "上一页";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(0, 1);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.btnPrev);
+            this.panel1.Location = new System.Drawing.Point(648, 430);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(156, 24);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Location = new System.Drawing.Point(0, 430);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(76, 24);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 454);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ draw_direct/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace draw_direct
+{
+    public partial class Form1 : Form
+    {
+        private int pageNo = 0;
+        private bool drawed = false;
+        private bool saved = false;
+        private string path;
+        private string root;
+        private bool working = false;
+        
+
+
+        private Point p1, p2;//定义两个点（启点，终点）
+
+        //private static bool drawing = false;//设置一个启动标志
+
+        private Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            root = Application.StartupPath;
+            Thread t = new Thread(Draw);
+            t.Start();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            //p1 = new Point(e.X, e.Y);
+            //p2 = new Point(e.X, e.Y);
+            //drawing = true;
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //drawing = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (drawed && !saved) {
+                var fileName = root + @"\" + path + @"\" + pageNo + ".png";
+                pictureBox1.BackgroundImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                drawed = false;
+                saved = false;
+                pageNo = 0;                
+            }
+            path = System.Guid.NewGuid().ToString();
+            g = pictureBox1.CreateGraphics();
+            g.Clear(pictureBox1.BackColor);
+            working = true;
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (drawed && !saved)
+            {
+                var fileName = root + @"\" + path + @"\" + pageNo + ".png";
+                pictureBox1.BackgroundImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                drawed = false;
+                saved = false;
+                pageNo++;
+            }
+
+            g = pictureBox1.CreateGraphics();
+            g.Clear(pictureBox1.BackColor);
+        }
+
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+
+        {
+
+            //if (e.Button == MouseButtons.Left)
+            //{
+            //    if (drawing)
+            //    {
+            //        //drawing = true;
+            //        Point currentPoint = new Point(e.X, e.Y);
+            //        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;//消除锯齿
+            //        g.DrawLine(new Pen(Color.Blue, 2), p2, currentPoint);
+
+            //        p2.X = currentPoint.X;
+            //        p2.Y = currentPoint.Y;
+            //    }
+
+            //}
+        }
+
+        private void Draw()
+        {
+            
+            //Point currentPoint = new Point(e.X, e.Y);
+            //g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;//消除锯齿
+            //g.DrawLine(new Pen(Color.Blue, 2), p2, currentPoint);
+
+            //p2.X = currentPoint.X;
+            //p2.Y = currentPoint.Y;
+        }
+    }   
+}
